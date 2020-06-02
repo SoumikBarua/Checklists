ceaa7d53c930ecd191214309431b052f63e2e213
SB <10300902+SoumikBarua@users.noreply.github.com>
2020-06-01 23:26:29 -0400

    Update data source methods, implement delegate methods

--- Checklists/ChecklistViewController.swift
+++ Checklists/ChecklistViewController.swift
@@ -18,13 +18,42 @@ class ChecklistViewController: UITableViewController {
     
     // MARK:-  Table View data source methods
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1
+        return 20
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "ChecklistItem", for: indexPath)
         
+        let label = cell.viewWithTag(1000) as! UILabel
+        if  indexPath.row % 5 == 0{
+            label.text = "Walk  the  dog"
+        } else if  indexPath.row % 5 == 1{
+            label.text = "Brush my teeth"
+        } else if indexPath.row % 5 == 2{
+            label.text =  "Learn iOS Development"
+        } else if  indexPath.row % 5 == 3{
+            label.text = "Soccer practice"
+        } else if indexPath.row % 5 ==  4{
+            label.text = "Eat ice cream"
+        }
+        
         return cell
     }
+    
+    // MARK:- Table View delegate methods
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        if let cell = tableView.cellForRow(at: indexPath) {
+            if cell.accessoryType == .checkmark {
+                cell.accessoryType = .none
+            } else if cell.accessoryType == .none {
+                cell.accessoryType = .checkmark
+            }
+        }
+        
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
+    
 }
 
