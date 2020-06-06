9c1904f4468c56ee61dbd6eb299e4d2d819c00e9
SB <10300902+SoumikBarua@users.noreply.github.com>
2020-06-05 23:57:17 -0400

    Add textfield outlet & update attributes, handle keyboard return/done button, disallow empty input, and implement textfield delegate methods to enable/disable Done button

--- Checklists/AddItemTableViewController.swift
+++ Checklists/AddItemTableViewController.swift
@@ -8,8 +8,12 @@
 
 import UIKit
 
-class AddItemTableViewController: UITableViewController {
-
+class AddItemTableViewController: UITableViewController, UITextFieldDelegate {
+    
+    @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var doneBarButton: UIBarButtonItem!
+    
+    // MARK:- View life cycles
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -21,6 +25,11 @@ class AddItemTableViewController: UITableViewController {
         
         // Turn off large navigation bar title
         navigationItem.largeTitleDisplayMode = .never
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        textField.becomeFirstResponder()
     }
     
     // MARK:- Table view delegate methods
@@ -38,5 +47,20 @@ class AddItemTableViewController: UITableViewController {
     @IBAction func done() {
         navigationController?.popViewController(animated: true)
     }
-
+    
+    // MARK:- Text field delegate methods
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        let oldText = textField.text!
+        let stringRange = Range(range, in: oldText)!
+        let newText = oldText.replacingCharacters(in: stringRange, with: string)
+        
+        doneBarButton.isEnabled = !newText.isEmpty
+        
+        return true
+    }
+    
+    func textFieldShouldClear(_ textField: UITextField) -> Bool {
+        doneBarButton.isEnabled = false
+        return true
+    }
 }
