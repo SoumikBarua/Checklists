f35c71ceb7ba91f11521ee76a6a2d3ea42f7dc47
SB <10300902+SoumikBarua@users.noreply.github.com>
2020-06-07 23:38:52 -0400

    Change cell design in storyboard, update configureCheckmark in ChecklistVC

--- Checklists/ChecklistViewController.swift
+++ Checklists/ChecklistViewController.swift
@@ -82,10 +82,12 @@ class ChecklistViewController: UITableViewController, AddItemTableViewController
     // This checks for current rows checked status and updates accessory view accordingly
     func configureCheckmark(for cell: UITableViewCell, with item: ChecklistItem) {
         
+        let label = cell.viewWithTag(1001) as! UILabel
+        
         if item.checked {
-            cell.accessoryType = .checkmark
+            label.text = "√"
         } else {
-            cell.accessoryType = .none
+            label.text = ""
         }
         
     }
