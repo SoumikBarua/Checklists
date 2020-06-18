8dd667921d6561a6826048086d6f21cdecca7da4
SB <10300902+SoumikBarua@users.noreply.github.com>
2020-06-17 23:32:02 -0400

    Implement creation of a default checklist for first run

--- Checklists/DataModel.swift
+++ Checklists/DataModel.swift
@@ -23,6 +23,7 @@ class DataModel {
     init() {
         loadChecklists()
         registerDefaults()
+        handleFirstTime()
     }
     
     // MARK:-  File operations
@@ -58,8 +59,22 @@ class DataModel {
     }
     
     func registerDefaults() {
-        let dictionary = ["ChecklistIndex": -1]
+        let dictionary = ["ChecklistIndex": -1, "FirstTime": true] as [String : Any]
         UserDefaults.standard.register(defaults: dictionary)
+    }
+    
+    func handleFirstTime() {
+        let userDefaults = UserDefaults.standard
+        let firstTime = userDefaults.bool(forKey: "FirstTime")
+        
+        if firstTime {
+            let checklist = Checklist(name: "List")
+            lists.append(checklist)
+            
+            indexOfSelectedChecklist  = 0
+            userDefaults.set(false, forKey: "FirstTime")
+            userDefaults.synchronize()
+        }
     }
     
 }
