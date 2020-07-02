1df58329ce75d846ce2664af6b1ac25973e9f015
SB <10300902+SoumikBarua@users.noreply.github.com>
2020-07-01 23:41:35 -0400

    Handle local notification events by conforming AppDelegate to UNUserNotificationCenterDelegate and remove notification test code

--- Checklists/AppDelegate.swift
+++ Checklists/AppDelegate.swift
@@ -10,7 +10,7 @@ import UIKit
 import UserNotifications
 
 @UIApplicationMain
-class AppDelegate: UIResponder, UIApplicationDelegate {
+class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterDelegate {
 
 
 
@@ -19,25 +19,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         
         // Notification authorization
         let center = UNUserNotificationCenter.current()
-        center.requestAuthorization(options: [.alert, .sound], completionHandler: {
-            granted, error in
-            if granted {
-                print("We have permission")
-            } else {
-                print("Permission denied")
-            }
-        })
-        
-        let content = UNMutableNotificationContent()
-        content.title = "Hello!"
-        content.body = "I am a local notificaiton"
-        content.sound = UNNotificationSound.default
-        
-        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 10, repeats: false)
-        
-        let request = UNNotificationRequest(identifier: "MyNotification", content: content, trigger: trigger)
-        
-        center.add(request)
+        center.delegate = self
         
         return true
     }
@@ -54,6 +36,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Called when the user discards a scene session.
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
+    }
+    
+    // MARK: - User Notification delegate methods
+    func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+        print("Received local notification \(notification)")
     }
 
 
